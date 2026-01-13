05b09aedb46cf2908fd6673cc5ecd96ddbb50fa9
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2026-01-13 11:22:05 +0900

    Boxをランダムで1000個作る

--- lib/rect02.ex
+++ lib/rect02.ex
@@ -1,8 +1,9 @@
-defmodule Rect01 do
+defmodule Rect02 do
+  # Boxをランダムで1000個作る
   use WebSockex
   require Logger
 
-  @port 57374
+  @port 55497
   @host "127.0.0.1"
 
   def start_link(_opts \\ []) do
@@ -20,12 +21,18 @@ defmodule Rect01 do
 
   def start() do
     {:ok, pid} = start_link()
-    add_slot(pid)
-    add_box_mesh(pid)
-    add_pbs_metallic(pid)
-    add_mesh_renderer(pid)
-    update_mesh_renderer(pid)
-    update_mesh_renderer(pid)
+    Enum.each(1..1000, fn _ -> box(pid) end)
+    Process.sleep(1000)
+  end
+
+  def box(pid) do
+    id = :rand.uniform(1_000_0000)
+    add_slot(pid, id)
+    add_box_mesh(pid, id)
+    add_pbs_metallic(pid, id)
+    add_mesh_renderer(pid, id)
+    update_mesh_renderer(pid, id)
+    update_mesh_renderer(pid, id)
   end
 
   def send_text(msg, pid) do
@@ -33,12 +40,12 @@ defmodule Rect01 do
     # Process.sleep(1000)
   end
 
-  def add_slot(pid) do
+  def add_slot(pid, id) do
     """
     {
       "$type" : "addSlot",
       "data" : {
-          "id" : "ymn_1",
+          "id" : "ymn_1_#{id}",
           "parent" : {
               "$type" : "reference",
               "targetId" : "Root"
@@ -50,9 +57,9 @@ defmodule Rect01 do
           "position" : {
               "$type" : "float3",
               "value" : {
-                  "x" : 0,
-                  "y" : 1.5,
-                  "z" : 10
+                  "x" : #{Enum.random(1..20)},
+                  "y" : #{Enum.random(1..20)},
+                  "z" : #{Enum.random(1..20)}
               }
           }
       }
@@ -61,13 +68,13 @@ defmodule Rect01 do
     |> send_text(pid)
   end
 
-  def add_box_mesh(pid) do
+  def add_box_mesh(pid, id) do
     """
     {
       "$type" : "addComponent",
-      "containerSlotId" : "ymn_1",
+      "containerSlotId" : "ymn_1_#{id}",
       "data" : {
-          "id" : "ymn_2",
+          "id" : "ymn_2_#{id}",
           "componentType" : "[FrooxEngine]FrooxEngine.BoxMesh"
       }
     }
@@ -75,13 +82,13 @@ defmodule Rect01 do
     |> send_text(pid)
   end
 
-  def add_pbs_metallic(pid) do
+  def add_pbs_metallic(pid, id) do
     """
     {
         "$type" : "addComponent",
-        "containerSlotId" : "ymn_1",
+        "containerSlotId" : "ymn_1_#{id}",
         "data" : {
-            "id" : "ymn_3",
+            "id" : "ymn_3_#{id}",
             "componentType" : "[FrooxEngine]FrooxEngine.PBS_Metallic"
         }
     }
@@ -89,18 +96,18 @@ defmodule Rect01 do
     |> send_text(pid)
   end
 
-  def add_mesh_renderer(pid) do
+  def add_mesh_renderer(pid, id) do
     """
     {
         "$type" : "addComponent",
-        "containerSlotId" : "ymn_1",
+        "containerSlotId" : "ymn_1_#{id}",
         "data" : {
-            "id" : "ymn_4",
+            "id" : "ymn_4_#{id}",
             "componentType" : "[FrooxEngine]FrooxEngine.MeshRenderer",
             "members": {
                 "Mesh": {
                     "$type": "reference",
-                    "targetId": "ymn_2"
+                    "targetId": "ymn_2_#{id}"
                 }
             }
         }
@@ -109,19 +116,19 @@ defmodule Rect01 do
     |> send_text(pid)
   end
 
-  def update_mesh_renderer(pid) do
+  def update_mesh_renderer(pid, id) do
     """
     {
         "$type" : "updateComponent",
         "data" : {
-            "id" : "ymn_4",
+            "id" : "ymn_4_#{id}",
             "members" : {
                 "Materials": {
                     "$type": "list",
                     "elements": [
                         {
                             "$type": "reference",
-                            "targetId": "ymn_3"
+                            "targetId": "ymn_3_#{id}"
                         }
                     ]
                 }
